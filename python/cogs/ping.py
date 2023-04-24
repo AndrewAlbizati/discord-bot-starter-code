49e6badeb790d8b037c012bcf13dd0bd3b856734
Andrew Albizati <aalbizati@sandiego.edu>
2023-04-23 22:07:19 -0700

    Changed class name:

--- python/cogs/ping.py
+++ python/cogs/ping.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 
 
-class HelpCommand(commands.Cog):
+class PingCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
